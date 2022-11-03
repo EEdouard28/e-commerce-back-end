2258385b04004f0083c27c421e91a6ae56d1a617
Evince Edouard <studioimac@evinces-imac.mynetworksettings.com>
2022-11-02 22:09:34 -0400

    category router worked on

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,18 +3,32 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
+  const allCategories = await Category.findAll().catch((err) => {
+    res.json(err);
+  });
   // be sure to include its associated Products
+  res.json(allCategories);
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
+  const catId = await Category.find().catch((err) => {
+    res.json(err);
+  });
   // be sure to include its associated Products
+  res.json(catId);
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const addCategory = await Category.create(req.body);
+    res.status(200).json(addCategory);
+  } catch (err) {
+    res.status(400),json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
